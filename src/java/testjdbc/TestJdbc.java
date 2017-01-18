d2b7d5b9d8b137562767484b6c42100d3d2c5cbf
Stanislav Popov <its.an.omen.man@gmail.com>
2017-01-18 23:10:51 +0200

    Added 2 xhtml pages(index and login(template)) with pages and css

--- src/java/testjdbc/TestJdbc.java
+++ src/java/testjdbc/TestJdbc.java
@@ -4,10 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import web.db.Database;
 
