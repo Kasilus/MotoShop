ac5dfc472186306095afc664c0208ccbc71745ec
Stanislav Popov <its.an.omen.man@gmail.com>
2017-01-23 22:29:36 +0200

    Added 2 login validations(first letter and prohibited words)

--- src/java/web/validators/LoginValidator.java
+++ src/java/web/validators/LoginValidator.java
@@ -1,6 +1,6 @@
-
 package web.validators;
 
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -10,18 +10,40 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.ValidatorException;
 
 @FacesValidator("web.validators.LoginValidator")
-public class LoginValidator implements Validator{
+public class LoginValidator implements Validator {
 
     @Override
-    public void validate(FacesContext context, UIComponent component, Object value){
-    
-        if(value.toString().length()<5){
-            ResourceBundle bundle = ResourceBundle.getBundle("web.nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage(bundle.getString("username_length_error"));
+    public void validate(FacesContext context, UIComponent component, Object value) {
+
+        ResourceBundle bundle = ResourceBundle.getBundle("web.nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+
+        String newValue = value.toString();
+
+        try {
+            if (newValue.length() < 5) {
+                throw new IllegalArgumentException(bundle.getString("username_length_error"));
+            }
+
+            if (!Character.isLetter(newValue.charAt(0))) {
+                throw new IllegalArgumentException(bundle.getString("username_first_letter"));
+            }
+
+            if (getTestArray().contains(newValue)) {
+                throw new IllegalArgumentException(bundle.getString("username_reserved"));
+            }
+        } catch (IllegalArgumentException e) {
+            FacesMessage message = new FacesMessage(e.getMessage());
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message);
         }
-    
+    }
+
+    private ArrayList<String> getTestArray() {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("username");
+        arr.add("login");
+        return arr;
+
     }
 
 }
