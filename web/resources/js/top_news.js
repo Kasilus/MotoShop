885b98bb146c12ca0cc1e954a17a5d6846987fee
Stanislav Popov <its.an.omen.man@gmail.com>
2017-01-31 14:48:43 +0200

    Adding changed top-news images with JS (not finished) Can't find correct path to folder /images

---
+++ web/resources/js/top_news.js
@@ -0,0 +1,14 @@
+function startTimer(){
+    setInterval(displayNextImage, 3000);
+}
+
+function displayNextImage(){
+    x = (x === images.length - 1) ? 0 : x+1;
+    document.getElementById("img").src = "${pageContext.request.contextPath}" + images[x];
+}
+
+var images = [], x=-1;
+images[0] = "resources/images/search.png.xhtml";
+images[1] = "images/back.png";
+
+
