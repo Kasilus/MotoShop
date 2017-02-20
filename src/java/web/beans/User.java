e31e835c46c4d1e96a0ebed82cd1c6bbc3b3e5e8
Stanislav Popov <its.an.omen.man@gmail.com>
2017-02-20 22:02:30 +0200

    Authorization for 2 groups: admin and user User,reader (user group) and admin (admin group) just can enter. Other lasts on the index.xhtml page. Roles are the same

--- src/java/web/beans/User.java
+++ src/java/web/beans/User.java
@@ -1,15 +1,31 @@
 package web.beans;
 
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @ManagedBean
 @SessionScoped
 public class User {
 
     private String username;
+    private String password;
 
     public User() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setUsername(String username) {
@@ -18,6 +34,27 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+    
+    public String login(){
+    
+        try {
+            System.out.println(username);
+            System.out.println(password);
+            ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).login(username, password);
+            
+            return "items";
+        } catch (ServletException ex) {
+            ResourceBundle bundle = ResourceBundle.getBundle("web.nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            FacesContext context = FacesContext.getCurrentInstance();
+            FacesMessage message = new FacesMessage(bundle.getString("username_and_password_not_true"));
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            context.addMessage("login_form", message);
+        }
+        
+        return "index";
+        
     }
 
 }
